6c979edbdb6642209ab9cee28043251613ff65ce
Ayush Mehta <ayushmehta651@gmail.com>
2023-05-29 23:29:43 +0530

    Add kafka commands

--- src/main/java/org/example/constants/constants.java
+++ src/main/java/org/example/constants/constants.java
@@ -1,7 +1,7 @@
 package org.example.constants;
 
 public class constants {
-    public static final String NEWS_URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey=XXXXX";
+    public static final String NEWS_URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey=67355a2134be42bd9c49f3757c0b9747";
     public static final String TOPIC = "news-topic";
-    public static final String BOOTSTRAP_SERVERS = "XXXX:9092";
+    public static final String BOOTSTRAP_SERVERS = "34.201.40.207:9092";
 }
